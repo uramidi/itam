91a8f75eb2faf25b7601c595e7c7fade3f5b66ba
Joe Davis <joe.davis@service-now.com>
2015-05-28 21:40:30 -0700

    Adding test projects

---
+++ example-scoped-app-test/src/test/java/zz_it/ZZ_TeardownIT.java
@@ -0,0 +1,11 @@
+package zz_it;
+
+import com.snc.glide.it.util.ZZ_SeleniumTeardown;
+
+/**
+ * This is the counterpart of AA_Setup. It shuts down any glide instance/node
+ * installed by AA_SetupIT
+ */
+public class ZZ_TeardownIT extends ZZ_SeleniumTeardown {
+
+}
